809c6f861e2aa1caa8b45263cfb7fdad9e796e8c
natngu18 <natngu18@gmail.com>
2024-06-12 13:27:22 -0400

    implemented optimistic update when user uploads new avatar

--- frontend/src/features/user/api/updateUser.ts
+++ frontend/src/features/user/api/updateUser.ts
@@ -1,7 +1,22 @@
-import { UpdateUserCommand } from './../types/index'
+import {
+    UpdateUserCommand,
+    UserBriefResponse,
+    UserDetailedResponse,
+} from './../types/index'
 import { auth } from '@/firebase/firebase'
 import { axiosInstance } from '@/lib/axios'
-import { useMutation, useQueryClient } from '@tanstack/react-query'
+import {
+    InfiniteData,
+    useMutation,
+    useQueryClient,
+} from '@tanstack/react-query'
+import { userQueryKeys } from '../queries'
+import { useAuth } from '@/features/auth/context/AuthContext'
+import localStorage from '@/lib/storage'
+import { UserResponse } from '@/features/auth/types'
+import { postQueryKeys } from '@/features/post/queries'
+import { PaginatedList } from '@/types'
+import { PostBriefResponse } from '@/features/post/types'
 
 export const updateUser = async (
     formData: FormData,
@@ -32,12 +47,108 @@ function createFormData(request: UpdateUserCommand): FormData {
 
 export const useUpdateUser = (username: string) => {
     const queryClient = useQueryClient()
+    const { firebaseUser } = useAuth()
     return useMutation({
         mutationFn: (request: UpdateUserCommand) =>
             updateUser(createFormData(request), username),
         // Always refetch after error or success:
-        onSettled: () => {
-            queryClient.invalidateQueries({ queryKey: ['user', username] })
+        onSettled: async (data, err, request) => {
+            await queryClient.invalidateQueries({
+                queryKey: userQueryKeys.detail(username),
+            })
+
+            // New user data after update from above query.
+            const newUserData =
+                await queryClient.getQueryData<UserDetailedResponse>(
+                    userQueryKeys.detail(username)
+                )
+
+            // if query fails, return
+            if (!newUserData) return
+            // If user updates image
+            if (request.avatar) {
+                // Update all user posts with new avatar
+                queryClient.setQueriesData(
+                    {
+                        queryKey: postQueryKeys.userLists(username),
+                        exact: false,
+                    },
+                    (
+                        oldData:
+                            | InfiniteData<PaginatedList<PostBriefResponse>>
+                            | undefined
+                    ) => {
+                        // If oldData is undefined, return undefined
+                        if (!oldData) {
+                            return undefined
+                        }
+                        return {
+                            ...oldData,
+                            pages: oldData.pages.map((page) => ({
+                                ...page,
+                                items: page.items.map((post) => {
+                                    return {
+                                        ...post,
+                                        author: {
+                                            ...post.author,
+                                            avatar: newUserData.avatar
+                                                ? newUserData.avatar
+                                                : post.author.avatar,
+                                        },
+                                    }
+                                }),
+                            })),
+                        }
+                    }
+                )
+
+                // Update post detail queries with new avatar
+                // Not most efficient way to do this, since we also invalidate posts not authored by current user,
+                // but a post query detail item is complex because it includes parent posts
+                await queryClient.invalidateQueries({
+                    queryKey: postQueryKeys.details(),
+                    exact: false,
+                    refetchType: 'inactive', // by default invalidateQueries() only forces refetch for active queries
+                })
+
+                // Update top users list with new avatar if user is in the list
+                const topUsers = queryClient.getQueryData<UserBriefResponse[]>(
+                    userQueryKeys.topUsers()
+                )
+                if (!topUsers) return
+                // Check if the user is in the top users list
+                const userIndex = topUsers?.findIndex(
+                    (user) => user.id === newUserData.id
+                )
+                // If the user is in the list, update their avatar
+                if (userIndex !== undefined && userIndex !== -1) {
+                    // Update the user's avatar in the top users list
+                    const updatedTopUsers = [...topUsers]
+                    updatedTopUsers[userIndex] = {
+                        ...updatedTopUsers[userIndex],
+                        avatar: newUserData.avatar,
+                    }
+                    // Update the query data
+                    queryClient.setQueryData(
+                        userQueryKeys.topUsers(),
+                        updatedTopUsers
+                    )
+                }
+            }
+
+            // Update current user data in localStorage and cache
+            // destructure not required fields
+            const {
+                followersCount,
+                followingsCount,
+                backgroundImage,
+                ...userResponse
+            } = newUserData
+            localStorage.setUser({ ...userResponse })
+            queryClient.setQueryData<UserResponse>(
+                ['user', firebaseUser?.uid],
+                { ...userResponse }
+            )
         },
     })
 }
